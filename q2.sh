9ab09be0ba8defc4a168838177b634dfd9ef0b46
antareep-bit <antareep.saud@niser.ac.in>
2019-08-09 07:22:24 -0400

    Done

--- q2.sh
+++ q2.sh
@@ -1,6 +1,6 @@
 #Write a shell script that asks user for its name. Creates a folder of his name and then creates 5 files inside that folder with his name and ending with numbers 1-5.cpp. 
 
-echo "Enter your name or I will haunt you"
+echo "Enter your name else I will haunt you"
 read var
 mkdir $var
 for i in 1 2 3 4 5
