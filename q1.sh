394e6dfade4585436bd606e0716f9a136b7d3ba9
antareep-bit <antareep.saud@niser.ac.in>
2019-08-09 07:29:20 -0400

    Done

--- q1.sh
+++ q1.sh
@@ -1,4 +1,4 @@
-#Q1: Write a shell script that asks user for its name. Create a folder of his name and then creates a file inside that folder with his name.cpp.
+#Q1: Write a shell script that asks user for its name. Creates a folder of his name and then creates a file inside that folder with his name.cpp.
 
 echo "For the wrath of Satan,Enter your Name"
 read var
--- q2.sh
+++ q2.sh
@@ -1,4 +1,4 @@
-#Write a shell script that asks user for its name. Creates a folder of his name and then creates 5 files inside that folder with his name and ending with numbers 1-5.cpp. 
+#Write a shell script that asks user for its name. Create a folder of his name and then creates 5 files inside that folder with his name and ending with numbers 1-5.cpp. 
 
 echo "Enter your name else I will haunt you"
 read var
